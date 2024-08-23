d03af092f9b9497c4f6e0a76be2a02a4ade2f293
pawan saini <saini.pawan1711.gmail.com>
2024-08-23 18:09:51 +0530

    first commit

---
+++ src/main/java/com/shopping/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+	public String index() {
+		return "index";
+	}
+    @GetMapping("/login")
+    public String login() {
+    	return "login";
+    }
+    @GetMapping("/register")
+    public String register() {
+    	return "register";
+    }
+   
+    
+    
+}
